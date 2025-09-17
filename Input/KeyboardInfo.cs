dd1b3c564c5ef501e43df024054e3339329d697a
CaliseNewton <newton@calise.dev>
2025-09-17 13:46:20 -0400

    Initial commit

---
+++ Input/KeyboardInfo.cs
@@ -0,0 +1,26 @@
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameLibrary.Input;
+
+public class KeyboardInfo
+{
+    /// <summary>
+    /// Gets the state of keyboard input during the previous update cycle.
+    /// </summary>
+    public KeyboardState PreviousState { get; private set; }
+
+    /// <summary>
+    /// Gets the state of keyboard input during the current input cycle.
+    /// </summary>
+    public KeyboardState CurrentState { get; private set; }
+
+    /// <summary>
+    /// Creates a new KeyboardInfo. 
+    /// </summary>
+    public KeyboardInfo()
+    {
+        PreviousState = new KeyboardState();
+        CurrentState = Keyboard.GetState();
+    }
+
+}
